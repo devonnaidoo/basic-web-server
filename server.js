1bbdb304428827c6e4db8f13eaf6fb3fb7f1956b
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-06 19:09:24 +0200

    Storing the files types for the project

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
+
+// Storing the files types for the project
+const fileTypes = {
+  html: "text/html",
+  css: "text/css",
+  js: "text/javascript",
+  jpg: "image/jpg",
+  jpeg: "image/jpeg",
+  png: "image/png"
+};
