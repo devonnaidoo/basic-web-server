9e23f18ae8658196c0309225fb8e28a1898a3474
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-06 19:33:42 +0200

    error handle added for 500 response

--- server.js
+++ server.js
@@ -14,34 +14,41 @@ const fileTypes = {
 };
 
 // Creating server
-http.createServer((req, res) => {
-  var uri = url.parse(req.url).pathname;
-  var fileName = path.join(process.cwd(), unescape(uri));
-  console.log(`Loading ` + uri);
-  var stats;
+http
+  .createServer((req, res) => {
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log(`Loading ` + uri);
+    var stats;
 
-  try {
-    stats = fs.lstatSync(fileName);
-  } catch (e) {
-    res.writeHead(404, { "Content-type": "text/plain" });
-    res.write("404 not found\n");
-    res.end();
-    return;
-  }
+    try {
+      stats = fs.lstatSync(fileName);
+    } catch (e) {
+      res.writeHead(404, { "Content-type": "text/plain" });
+      res.write("404 not found\n");
+      res.end();
+      return;
+    }
 
-  if (stats.isFile()) {
-    var mimeType =
-      mimeTypes[
-        path
-          .extname(fileName)
-          .split(".")
-          .reserves()[0]
-      ];
-    res.writeHead(200, { "Content-type": mimeType });
+    if (stats.isFile()) {
+      var mimeType =
+        mimeTypes[
+          path
+            .extname(fileName)
+            .split(".")
+            .reserves()[0]
+        ];
+      res.writeHead(200, { "Content-type": mimeType });
 
-    var fileStream = fs.createReadStream(fileName);
-    fileStream.pipe(res);
-  } else if (stats.isDirectory()) {
-    res.writeHead(302, { Location: "index.html" });
-  }
-});
+      var fileStream = fs.createReadStream(fileName);
+      fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+      res.writeHead(302, { Location: "index.html" });
+      res.end();
+    } else {
+      res.writeHead(200, { "Content-type": "text/plain" });
+      res.write("500 Internal Error\n");
+      res.end();
+    }
+  })
+  .listen(1337);
